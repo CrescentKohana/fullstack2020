1584629ce1d37f5e57b1673c21167090ed34bec3
Luukuton <mleinikka@gmail.com>
2020-09-03 18:00:52 +0300

    Part 2 started and exercises 2.1-2.5 done

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Header = ({ name }) => {
+    return (
+      <h2>{name}</h2>
+    )
+  }
+  
+  const Total = ({ parts }) => {
+    const total = parts.reduce((sum, current) => sum + current.exercises, 0)
+  
+    return (
+      <p>Number of exercises {total}</p>
+    )
+  }
+  
+  const Part = ({ part }) => {
+    return (
+      <p>{part.name} {part.exercises}</p>
+    )
+  }
+  
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        {parts.map(part => 
+          <Part key={part.id} part={part} />
+        )}
+      </div>
+    )
+  }
+  
+  const Course = ({ course }) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+  }
+
+export default Course
